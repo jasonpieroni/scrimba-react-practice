22531e69339f7bfe75e7ce453791a6c72a94459c
jasonpieroni <jasonpieroni@gmail.com>
2023-05-22 19:45:46 -0400

    added all files to main

---
+++ src/NavBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NavBar() {
+    return (
+    <nav>
+        <h3>React Facts</h3>
+        <br />
+        <h4>React Course - Project 1</h4>
+    </nav>
+    )
+}
+export default NavBar
